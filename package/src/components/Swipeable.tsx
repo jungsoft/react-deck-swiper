a481583f2ba08650b66819a82bbeefa5f5ff143c
Pedro Bini <dev.pedrobini@gmail.com>
2020-03-04 17:54:39 -0300

    Use useSpring & animated instead of Spring

--- package/src/components/Swipeable.tsx
+++ package/src/components/Swipeable.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { Spring } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 
 // eslint-disable-next-line no-unused-vars
-import { RenderButtonsPayload, SwipeableWrapperProps, SwipeableState } from './SwipeableWrapper';
+import { SwipeableWrapperProps, SwipeableState } from './SwipeableWrapper';
 
 import directionEnum from '../constants/direction';
 import { getOpacity } from '../utils/helpers';
@@ -35,42 +35,51 @@ const Swipeable = ({
   min = 40,
   children,
   state,
-}: SwipeableProps) => (
-  <>
-    <Spring
-      from={{ offset: 0, opacity: 1 }}
-      to={{
-        opacity: getOpacity(state.offset, limit, min),
-        offset: state.offset,
-      }}
-      immediate={state.pristine || (!state.forced && Math.abs(state.offset) >= limit)}
-      onRest={() => state.swiped && handleOnAfterSwipe()}
-      config={SWIPE_CONFIG}
-    >
-      {({ offset, opacity }) => (
-        <div
-          style={{
-            opacity,
-            transform: `translateX(${offset}px) rotate(${offset / 10}deg)`,
-            height: wrapperHeight,
-            width: wrapperWidth,
-          }}
-          onTouchStart={handleOnDragStart}
-          onMouseDown={handleOnDragStart}
-        >
-          {children}
-        </div>
-      )}
-    </Spring>
-    {
-      renderButtons && (
-        renderButtons({
-          right: () => handleForceSwipe(directionEnum.RIGHT),
-          left: () => handleForceSwipe(directionEnum.LEFT),
-        })
-      )
-    }
-  </>
-);
+}: SwipeableProps) => {
+  const {
+    opacity,
+    offset,
+  } = useSpring({
+    immediate: state.pristine || (!state.forced && Math.abs(state.offset) >= limit),
+    onRest: () => state.swiped && handleOnAfterSwipe(),
+    config: SWIPE_CONFIG,
+    from: {
+      opacity: 1,
+      offset: 0,
+    },
+    to: {
+      opacity: getOpacity(state.offset, limit, min),
+      offset: state.offset,
+    },
+  });
+
+  const animatedStyle = {
+    transform: `translateX(${offset}px) rotate(${parseFloat(String(offset)) / 10}deg)`,
+    height: wrapperHeight,
+    width: wrapperWidth,
+    opacity,
+  };
+
+  return (
+    <>
+      <animated.div
+        onTouchStart={handleOnDragStart}
+        onMouseDown={handleOnDragStart}
+        style={animatedStyle}
+      >
+        {children}
+      </animated.div>
+
+      {
+        renderButtons && (
+          renderButtons({
+            right: () => handleForceSwipe(directionEnum.RIGHT),
+            left: () => handleForceSwipe(directionEnum.LEFT),
+          })
+        )
+      }
+    </>
+  );
+};
 
 export default Swipeable;
