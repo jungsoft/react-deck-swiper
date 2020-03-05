5dded96b60f926c0cdaecea3594e9bb0bd6f25f0
Pedro Bini <dev.pedrobini@gmail.com>
2020-03-05 01:05:45 -0300

    Updates

---
+++ precache-manifest.c5e79a2d2fbda552ae006a23c7fe2569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcf631ac01d2909f6efc3e5e627f9b8",
+    "url": "/react-deck-swiper/index.html"
+  },
+  {
+    "revision": "6d8501d9d59f3761b6c7",
+    "url": "/react-deck-swiper/static/js/2.2ce1cf8a.chunk.js"
+  },
+  {
+    "revision": "647c25ca9e3ea0b71bfc55fa250b5834",
+    "url": "/react-deck-swiper/static/js/2.2ce1cf8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9354a83ed3236d65f489",
+    "url": "/react-deck-swiper/static/js/main.c2b93a33.chunk.js"
+  },
+  {
+    "revision": "c12b581e371e94724eb2",
+    "url": "/react-deck-swiper/static/js/runtime-main.202a2bcf.js"
+  }
+]);
